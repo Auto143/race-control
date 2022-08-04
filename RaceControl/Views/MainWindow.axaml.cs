6aa6d4a4e6d8f8fafa462248b636b580f34d224d
Auto143 <110412370+Auto143@users.noreply.github.com>
2022-08-04 21:51:10 +0100

    Setup AvaloniaUI project and installed required packages

---
+++ RaceControl/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace RaceControl.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
