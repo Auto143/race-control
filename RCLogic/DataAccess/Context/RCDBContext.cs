b39716ed524fe17d425a1c1d246b35b132cc4d05
Auto143 <110412370+Auto143@users.noreply.github.com>
2022-08-06 11:32:57 +0100

    Created RaceControl SQLite database context

---
+++ RCLogic/DataAccess/Context/RCDBContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCLogic.DataAccess.Context
+{
+    internal class RCDBContext : DbContext
+    {
+        private const string _appDBFolderStructure = "/RaceControl/Database/";
+        private const string _dbName = "racecontrol";
+        private string _dbPath;
+
+        internal RCDBContext()
+        {
+            Environment.SpecialFolder appDataFolder = Environment.SpecialFolder.LocalApplicationData;
+            string appDataFolderPath = Environment.GetFolderPath(appDataFolder);
+
+            string dbFolderPath = Path.Join(appDataFolderPath, _appDBFolderStructure);
+
+            if (!Directory.Exists(dbFolderPath))
+            {
+                Directory.CreateDirectory(dbFolderPath);
+            }
+
+            _dbPath = Path.Join(dbFolderPath, _dbName);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={_dbPath}");
+    }
+}
