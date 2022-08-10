a6ec336bd1ae39ae2c45043f8e356ab7990dec6e
Auto143 <110412370+Auto143@users.noreply.github.com>
2022-08-10 22:54:37 +0100

    Renamed data service as included additional information not needed

---
+++ RCDataAccess/Services/Implementations/SQLite/DataService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RCDataAccess.Contexts;
+using RCDataAccess.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCDataAccess.Services.Implementations.SQLite
+{
+    public class DataService : IDataService
+    {
+        public IMeetService Meet { get; }
+
+        public ISeriesService Series { get; }
+
+        internal RCSQLiteContext rcSQLiteContext { get; }
+
+        public DataService(string dbName, string appDBFolderStucture)
+        {
+            rcSQLiteContext = new RCSQLiteContext(dbName, appDBFolderStucture);
+
+            rcSQLiteContext.Database.EnsureCreated();
+
+            Series = new SeriesService(rcSQLiteContext);
+        }
+
+        public void DeleteSource()
+        {
+            rcSQLiteContext.Database.EnsureDeleted();
+        }
+
+        public void Dispose()
+        {
+            rcSQLiteContext.Dispose();
+        }
+    }
+}
