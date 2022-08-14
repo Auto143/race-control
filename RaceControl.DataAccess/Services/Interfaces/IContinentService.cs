0b534de9b2e688b4be1b3e01113ed18389d272a4
Auto143 <110412370+Auto143@users.noreply.github.com>
2022-08-14 21:24:51 +0100

    Created continent service interface

---
+++ RaceControl.DataAccess/Services/Interfaces/IContinentService.cs
@@ -0,0 +1,24 @@
+﻿using RaceControl.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceControl.DataAccess.Services.Interfaces
+{
+    public interface IContinentService
+    {
+        public bool CheckExists(string continentCode);
+
+        public Continent Get(string continentCode);
+
+        public List<Continent> GetAll();
+
+        public Continent CreateNew(string continentCode);
+
+        public void Update(Continent continent);
+
+        public void Delete(string continentCode);
+    }
+}
