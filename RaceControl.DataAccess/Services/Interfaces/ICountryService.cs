ede2baa8985c7eabe94c62bb55ffc2bb344ac17a
Auto143 <110412370+Auto143@users.noreply.github.com>
2022-08-14 21:59:56 +0100

    Created country service interface

---
+++ RaceControl.DataAccess/Services/Interfaces/ICountryService.cs
@@ -0,0 +1,26 @@
+﻿using RaceControl.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceControl.DataAccess.Services.Interfaces
+{
+    public interface ICountryService
+    {
+        public bool CheckExists(string countryCode);
+
+        public Country Get(string countryCode);
+
+        public List<Country> GetAll();
+
+        public List<Country> GetAllInContinent(string continentCode);
+
+        public Country CreateNew(string countryCode);
+
+        public void Update(Country country);
+
+        public void Delete(string countryCode);
+    }
+}
