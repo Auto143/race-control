6dd4d6c3ab93f4e80b88e853d58bd2c884cdd70a
Auto143 <110412370+Auto143@users.noreply.github.com>
2022-08-28 23:20:05 +0100

    Remove not needed using directives

--- RaceControl.DataAccess/Services/Interfaces/IContinentService.cs
+++ RaceControl.DataAccess/Services/Interfaces/IContinentService.cs
@@ -1,9 +1,4 @@
 ﻿using RaceControl.DataAccess.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RaceControl.DataAccess.Services.Interfaces
 {
